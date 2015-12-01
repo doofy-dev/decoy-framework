d853024f4cce09bb4bb10af1ff623f963d0e5c73
teeebor <teeebor@gmail.com>
2015-12-01 15:00:25 +0100

    Logger error fixed

--- log/Logger.php
+++ log/Logger.php
@@ -20,23 +20,11 @@ class Logger
 	 */
 	public static function Log($file, $message)
 	{
-		if(!file_exists(realpath($file)))
-			mkdir(realpath($file),0755,true);
+		$path = dirname(dirname(dirname(dirname(__DIR__)))).'/'.$file;
+		if(!file_exists(dirname($path)))
+			mkdir(dirname($path),0755,true);
 		$date = new \DateTime();
-		file_put_contents($file,"\r\n".'['.$date->format('Y-m-d H:i:s').'] '.$message,FILE_APPEND);
+		file_put_contents($path,"\r\n".'['.$date->format('Y-m-d H:i:s').'] '.$message,FILE_APPEND);
 	}
 
-	/**
-	 * @param $path
-	 */
-	public static function mkdirs($path)
-	{
-		$expl = explode('/(\/|\\)/i', $path);
-		$curr = '';
-		for ($i = 0; $i < count($expl) - 1; $i++) {
-			if (!file_exists($expl[$i]))
-				mkdir($curr . '/' . $expl[$i], 0755);
-			$curr .= '/' . $expl[$i];
-		}
-	}
 }
